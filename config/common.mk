5a8316d50627109a2f1ea06f2cb363aec6456440
OliverSyx <173170056+OliverSyx@users.noreply.github.com>
2025-10-22 14:39:35 +0000

    vendor: Update path gms services
    Change-Id: Id0e29d404b75ea9486c0de25d777ec65af701e2a
    Signed-off-by: OliverSyx <173170056+OliverSyx@users.noreply.github.com>

--- config/common.mk
+++ config/common.mk
@@ -9,7 +9,7 @@ $(call inherit-product-if-exists, vendor/rising-prebuilts/prebuilts_product_copy
 
 # Define some properties for GMS
 ifeq ($(WITH_GMS), true)
-$(call inherit-product-if-exists, vendor/gms/products/gms.mk)
+$(call inherit-product-if-exists, vendor/gms/common.mk)
 endif
 
 PRODUCT_BRAND ?= RisingOS
